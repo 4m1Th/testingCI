6868e1498e9b306964f3096aedb6da0f0f7b87c2
4m1Th <amithc38@gmail.com>
2021-04-25 20:27:55 +0530

    changed the fb url www.facebook.com

--- src/main/java/input/data.java
+++ src/main/java/input/data.java
@@ -1,7 +1,7 @@
 package input;
 
 public class data {
-	public String facebook_url = "https://facebook.com/";
+	public String facebook_url = "https://www.facebook.com/";
 	public String username="danwand47@gmail.com";
 	public String password="cz12cz12cz12";
 	public String test="test";
